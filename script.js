4716156ad912adde508275d5391c7d729ed2d16c
Eliyahu Shlieserman <eliyahushli@gmail.com>
2018-06-12 14:46:21 +0300

    Add Google search from search box via JS

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+function googleSearch() {
+    var q = document.getElementById("search").value;
+
+    window.open("https://www.google.com/search?q=" + q, "_blank");
+}
+
 function changeSlide() {
     var current = -1;
     var slides = document.getElementsByClassName("slide");
-    for (var i = 0; i < slides.length; i++) {
-        var s = slides[i];
-        if (s.classList.contains("active")) {
-            current = i;
-            s.classList.remove("active");
+    if (slides.length > 0) {
+        for (var i = 0; i < slides.length; i++) {
+            var s = slides[i];
+            if (s.classList.contains("active")) {
+                current = i;
+                s.classList.remove("active");
+            }
         }
-    }
 
-    if (current == 2) {
-        current = 0;
-    } else { 
-        current++;
-    }
+        if (current == 2) {
+            current = 0;
+        } else {
+            current++;
+        }
 
-    slides[current].classList.add("active");
+        slides[current].classList.add("active");
+    }
 }
 
 setInterval(changeSlide, 5000);
